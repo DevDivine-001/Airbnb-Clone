bc2526db364d6b34bfcbe271a14816e42ada3d9b
Oscdivin <divineuwandu84@Gmail.com>
2024-10-08 12:32:26 +0100

    label

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -3,6 +3,7 @@ import Container from '../Container'
 import { TbBeach } from "react-icons/tb"
 import { GiWindmill} from "react-icons/gi"
 import { MdOutlineVilla} from "react-icons/md"
+import CategoryBox from '../CategoryBox'
 
 export const categories = [
     {
@@ -34,6 +35,16 @@ const Categories = () => {
         items-center
         justify-between
         overflow-x-auto'>
+            {
+                categories.map((item) =>(
+                    <CategoryBox
+                    key={item.label}
+                    label={item.label}
+                    description={item.description}
+                    icon={item.icon}
+                    />
+                ))
+            }
 
         </div>
     </Container>
