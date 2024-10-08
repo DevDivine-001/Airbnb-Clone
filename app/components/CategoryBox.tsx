48a02dffe1fd1d87d80f99487a88de72c5e4a987
Oscdivin <divineuwandu84@Gmail.com>
2024-10-08 13:26:25 +0100

    selected category

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -1,6 +1,8 @@
 import { useRouter, useSearchParams } from 'next/navigation'
 import React, { useCallback } from 'react'
 import { IconType } from "react-icons"
+import qs from "query-string"
+
 
 interface CategoryBoxProps{
     icon: IconType,
@@ -17,10 +19,31 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
     const params = useSearchParams()
 
     const handleClick = useCallback(() =>{
+        let currentQuery = {}
 
-    }, [])
+        if(params){
+            currentQuery = qs.parse(params.toString())
+        }
+        const updatedQuery: any = {
+            ...currentQuery,
+            category:label
+        }
+
+        if(params?.get('category') === label) {
+            delete updatedQuery.category
+        }
+
+        const url = qs.stringifyUrl({
+            url:"/",
+            query: updatedQuery,
+        }, {skipNull: true});
+
+        router.push(url)
+
+    }, [label, params, router])
   return (
     <div
+    onClick={handleClick}
     className={`
         flex
         flex-col
--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -4,6 +4,7 @@ import { TbBeach } from "react-icons/tb"
 import { GiWindmill} from "react-icons/gi"
 import { MdOutlineVilla} from "react-icons/md"
 import CategoryBox from '../CategoryBox'
+import { usePathname, useSearchParams } from 'next/navigation'
 
 export const categories = [
     {
@@ -25,6 +26,15 @@ export const categories = [
 ]
 
 const Categories = () => {
+    const params = useSearchParams()
+    const category = params?.get('category')
+    const pathname = usePathname()
+
+    const isMainPage = pathname === '/' 
+
+    if(!isMainPage){
+        return null
+    }
   return (
     <Container>
         <div
@@ -41,7 +51,7 @@ const Categories = () => {
                     <CategoryBox
                     key={item.label}
                     label={item.label}
-                    description={item.description}
+                    selected={category === item.label}
                     icon={item.icon}
                     />
                 ))
