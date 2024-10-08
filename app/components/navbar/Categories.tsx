5bee16bf14e1904af8f767208cea1770103580fd
Oscdivin <divineuwandu84@Gmail.com>
2024-10-08 12:24:58 +0100

    some Categories like Modern Giindnill and Barch

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import Container from '../Container'
+import { TbBeach } from "react-icons/tb"
+import { GiWindmill} from "react-icons/gi"
+import { MdOutlineVilla} from "react-icons/md"
+
+export const categories = [
+    {
+        label:'Beach',
+        icon: TbBeach,
+        description:"This property is close to the beach",
+
+    },
+    {
+          label:'GiWindmill',
+        icon:GiWindmill,
+        description:"This property has GiWindmill"
+    },
+    {
+          label:'Modern',
+        icon:MdOutlineVilla,
+        description:"This property is Modern"
+    },
+]
 
 const Categories = () => {
   return (
-    <div>Categories</div>
+    <Container>
+        <div
+        className='
+        pt-4
+        flex
+        flex-row
+        items-center
+        justify-between
+        overflow-x-auto'>
+
+        </div>
+    </Container>
   )
 }
 
