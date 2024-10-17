a1852949f575bc02f5f1bc5f17035464436ee1ad
Oscdivin <divineuwandu84@Gmail.com>
2024-10-17 08:20:23 +0100

    map search

---
+++ app/components/Map.tsx
@@ -0,0 +1,102 @@
+// 'use client'
+
+
+// import React from 'react'
+
+// import L from 'leaflet';
+
+// import { MapContainer, Marker, TileLayer} from 'react-leaflet'
+
+// import "leaflet/dist/leaflet.css"
+// import markerIcon2z from "leaflet/dist/images/marker-icon-2x.png"
+// import markerIcon from "leaflet/dist/images/marker-icon.png"
+// import markerShadow from "leaflet/dist/images/marker-shadow.png"
+
+// delete L.Icon.Default.prototype._getIconUrl;
+// L.Icon.Default.mergeOptions({
+//     iconUrl:markerIcon.src,
+//     iconRetinaUrl: markerIcon2z.src,
+//     shadowUrl: markerShadow.src
+// })
+
+// interface MapProps{
+//     center?: number[]
+// }
+// const Map:React.FC<MapProps> = ({
+//     center
+// }) => {
+//   return (
+//     <MapContainer
+//     center={center as L.LatLngExpression || [51, -0.09]}
+//     zoom={center ? 4:2}
+//     scrollWheelZoom={false}
+//     className='h-[40vh] rounded-lg'
+//     >
+
+//          <TileLayer
+  
+//       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//     />
+    
+//     </MapContainer>
+//   )
+// }
+
+// export default Map
+
+
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import L from 'leaflet';
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon.src,
+  iconRetinaUrl: markerIcon2x.src,
+  shadowUrl: markerShadow.src,
+});
+
+interface MapProps {
+  center?: number[];
+}
+
+const Map: React.FC<MapProps> = ({ center }) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+   
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+ 
+    return null;
+  }
+
+  return (
+    <MapContainer
+      center={center as L.LatLngExpression || [51, -0.09]}
+      zoom={center ? 4 : 2}
+      scrollWheelZoom={false}
+      className="h-[40vh] rounded-lg border-none"
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {
+        center &&(
+            <Marker
+            position={center as L.LatLngExpression}/>
+        )
+      }
+   
+    </MapContainer>
+  );
+};
+
+export default Map;
--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -8,6 +8,8 @@ import {categories} from '../navbar/Categories'
 import CategroyInput from '../input/CategroyInput'
 import { FieldValues, useForm } from 'react-hook-form'
 import CountrySelect from '../input/CountrySelect'
+import Map from '../Map'
+import dynamic from 'next/dynamic'
 
 enum STEPS {
     CATEGORY = 0,
@@ -52,6 +54,12 @@ const RentModal = () => {
     })
 
     const category = watch('category')
+    const location = watch('location')
+
+    const Map = useMemo(() => dynamic(() => import('../Map'), {
+        ssr: false
+
+    }), [location])
 
     const setCustomValue = (id: string, value:any) =>{
         setValue(id, value, {
@@ -134,7 +142,11 @@ const RentModal = () => {
                    subtitle='Help guests fin you!'
                    />
 
-                   <CountrySelect/>
+                   <CountrySelect 
+                   value={location}
+                   onChange={(value) => setCustomValue("location", value)}
+                   />
+                   <Map center={location?.latlng}/>
                 </div>
             )
 
