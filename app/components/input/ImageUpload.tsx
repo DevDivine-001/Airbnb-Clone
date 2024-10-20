12aa00224c2bac792a75c85065cbe4062d3fd9e0
Oscdivin <divineuwandu84@Gmail.com>
2024-10-20 22:05:15 +0100

    your-upload-preset-name

--- app/components/input/ImageUpload.tsx
+++ app/components/input/ImageUpload.tsx
@@ -1,10 +1,154 @@
 'use clients'
 import React from 'react'
 
-const ImageUpload = () => {
+import { CldUploadWidget} from "next-cloudinary"
+import Image from "next/image"
+import { useCallback } from "react"
+import { TbPhotoPlus } from "react-icons/tb"
+
+declare global {
+  var cloudinary : any;
+}
+
+interface ImageUploadProps{
+  onChange : (value: string) => void
+  value: string
+}
+
+const ImageUpload:React.FC<ImageUploadProps> = ({
+  onChange,
+  value
+}) => {
+
+  const handleUpload = useCallback((result:any) =>{
+    onChange(result.info.secure_url)
+  }, [onChange])
   return (
-    <div>ImageUpload</div>
+    <CldUploadWidget
+    onUpload={handleUpload}
+    uploadPreset='your-upload-preset-name'
+    options={{
+      maxFiles:1
+    }}
+    >
+      {({open}) =>{
+        return(
+          <div
+          onClick={() => open?.()}
+          className='
+          relative
+          cursor-pointer
+          transition
+          border-dashed
+          border-2
+          p-20
+          border-neutral-300
+          flex
+          flex-col
+          justify-center
+          items-center
+          gap-4
+          text-neutral-600'
+          >
+            <TbPhotoPlus size={50}/>
+            <div className='font-semibold text-lg'>
+              Click to Upload
+            </div>
+            {
+              value &&  (
+                <div className='absolute inset-0 w-full h-full'>
+                  <Image
+                  alt='Upload'
+                  fill
+                  style={{objectFit: 'cover'}}
+                  src={value}
+                  />
+
+                </div>
+              )
+            }
+
+          </div>
+        )
+      }}
+      </CldUploadWidget>
   )
 }
 
 export default ImageUpload
+
+
+
+// 'use client'
+// import React, { useCallback } from 'react';
+// import { CldUploadWidget } from "next-cloudinary";
+// import Image from "next/image";
+// import { TbPhotoPlus } from "react-icons/tb";
+
+// declare global {
+//   var cloudinary: any;
+// }
+
+// interface ImageUploadProps {
+//   onChange: (value: string) => void;
+//   value: string;
+// }
+
+// const ImageUpload: React.FC<ImageUploadProps> = ({
+//   onChange,
+//   value
+// }) => {
+
+//   const handleUpload = useCallback((result: any) => {
+//     onChange(result.info.secure_url);
+//   }, [onChange]);
+
+//   return (
+//     <CldUploadWidget
+//       onUpload={handleUpload}
+//       uploadPreset='your-upload-preset-name'  // Add your Cloudinary preset name here
+//       options={{
+//         maxFiles: 1
+//       }}
+//     >
+//       {({ open }) => {
+//         return (
+//           <div
+//             onClick={() => open?.()}
+//             className='
+//             relative
+//             cursor-pointer
+//             transition
+//             border-dashed
+//             border-2
+//             p-20
+//             border-neutral-300
+//             flex
+//             flex-col
+//             justify-center
+//             items-center
+//             gap-4
+//             text-neutral-600'
+//           >
+//             <TbPhotoPlus size={50} />
+//             <div className='font-semibold text-lg'>
+//               Click to Upload
+//             </div>
+//             {value && (
+//               <div className='absolute inset-0 w-full h-full'>
+//                 <Image
+//                   alt='Upload'
+//                   fill
+//                   style={{ objectFit: 'cover' }}
+//                   src={value}
+//                 />
+//               </div>
+//             )}
+//           </div>
+//         );
+//       }}
+//     </CldUploadWidget>
+//   );
+// }
+
+// export default ImageUpload;
