809affeec02cdaa0caab51ba62531198c4dab264
Oscdivin <divineuwandu84@Gmail.com>
2024-10-28 03:07:35 +0100

    put me and images getListingByid

--- app/actions/getListingByid.ts
+++ app/actions/getListingByid.ts
@@ -1,43 +1,43 @@
-// import prisma from '@/app/libs/primadb'
+import prisma from '@/app/libs/primadb'
 
 
-// interface IParams {
-//     listingId?: string
-// }
+interface IParams {
+    listingId?: string
+}
 
 
-// export default async function getListingById(params: IParams) {
-//     try {
-//         const { listingId } = params
+export default async function getListingById(params: IParams) {
+    try {
+        const { listingId } = params
 
-//         const listing = await prisma.listing.findUnique({
-//             where: {
-//                 id: listingId
-//             },
-//             include: {
-//                 user: true
-//             }
-//         })
+        const listing = await prisma.listing.findUnique({
+            where: {
+                id: listingId
+            },
+            include: {
+                user: true
+            }
+        })
 
-//         if (!listing) {
-//             return null
-//         }
+        if (!listing) {
+            return null
+        }
 
-//         return {
-//             ...listing,
-//             createdAt: listing.createdAt.toISOString(),
-//             user: {
-//                 ...listing.user,
-//                 createdAt: listing.createdAt.toISOString(),
-//                 updatedAt: listing.user.updatedAt.toISOString(),
-//                 emailVerified: listing.user.emailVerified?.toISOString() || null
-//             }
-//         }
+        return {
+            ...listing,
+            createdAt: listing.createdAt.toISOString(),
+            user: {
+                ...listing.user,
+                createdAt: listing.createdAt.toISOString(),
+                updatedAt: listing.user.updatedAt.toISOString(),
+                emailVerified: listing.user.emailVerified?.toISOString() || null
+            }
+        }
 
 
 
-//     } catch (error: any) {
+    } catch (error: any) {
 
-//     }
+    }
 
-// }
+}
