9dd8a69c475d62bb475f437a9f33f11f8a5958e2
Oscdivin <divineuwandu84@Gmail.com>
2024-10-28 12:53:09 +0100

    ListingPage filed

--- app/listings/[listingid]/page.tsx
+++ app/listings/[listingid]/page.tsx
@@ -1,30 +1,29 @@
+import getListingById from '@/app/actions/getListingByid';
+import ClientOnly from '@/app/components/ClientOnly';
+import EmptyState from '@/app/components/EmptyState';
 
-// import getListingById from '@/app/actions/getListingByid';
-// import ClientOnly from '@/app/components/ClientOnly';
-// import EmptyState from '@/app/components/EmptyState';
+interface ListingPageProps {
+    params: { listingid: string }; // Define the type of params
+}
 
-// interface ListingPageProps {
-//     params: { listingid: string }; // Define the type of params
-// }
+const ListingPage = async ({ params }: ListingPageProps) => {
+    const { listingid } = params; // Extract listingid from params
 
-// const ListingPage = async ({ params }: ListingPageProps) => {
-//     const { listingid } = params; // Extract listingid from params
+    const listing = await getListingById({ listingId:listingid }); 
 
-//     const listing = await getListingById({ listingId:listingid }); 
-
-//     // Ensure the listing ID is valid
-//     if (!listing) {
-//         return (<ClientOnly>
-//             <EmptyState/>
-//         </ClientOnly>); // Handle the case of no ID
-//     }
-//         return (
-//            <div>{listing.title}{/* Render other listing details */} </div>
+    // Ensure the listing ID is valid
+    if (!listing) {
+        return (<ClientOnly>
+            <EmptyState/>
+        </ClientOnly>); // Handle the case of no ID
+    }
+        return (
+           <div>{listing.title}{/* Render other listing details */} </div>
              
-//         );
+        );
  
 
  
-// };
+};
 
-// export default ListingPage;
+export default ListingPage;
