8d0f0c3b8f3fefd55fd0b54737ede8e665402744
Oscdivin <divineuwandu84@Gmail.com>
2024-10-30 20:04:10 +0100

    Trips for client in listings

--- app/trips/TripsClient.tsx
+++ app/trips/TripsClient.tsx
@@ -1,8 +1,38 @@
+'use client'
 import React from 'react'
+import Heading from '../components/Heading'
+import { SafeReservation, SafeUser } from '../types/inex'
+import Container from '../components/Container'
+import { useRouter } from 'next/navigation'
 
-const TripsClient = () => {
+interface TripsClientProps{
+  reservations: SafeReservation[]
+  currentUser?: SafeUser | null
+} 
+
+const TripsClient: React.FC<TripsClientProps> = ({
+  reservations,
+  currentUser
+}) => {
   return (
-    <div>My TripsClient</div>
+    <Container>
+      <Heading
+      title='Trips'
+      subtitle="Where you've been and where you're going"
+      />
+      <div className='
+      mt-10
+      grid
+      grid-cols-1
+      sm:grid-cols-2
+      md:grid-cols-3
+      lg:grid-cols-4
+      xl:grid-cols-5
+      2xl:grid-cols-6
+      gap-8'>
+TripsPages
+      </div>
+    </Container>
   )
 }
 
