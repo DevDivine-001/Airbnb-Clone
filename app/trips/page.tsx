befa5601ebb24846bf951009198c4f465efde5e0
Oscdivin <divineuwandu84@Gmail.com>
2024-11-01 22:50:10 +0100

    TripsClient page

--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -1,53 +1,53 @@
-// import getCurrentUser from "../actions/getCurrentUser"
-// import getReservations from "../actions/getReservations"
-// import ClientOnly from "../components/ClientOnly"
-// import EmptyState from "../components/EmptyState"
-// import TripsClient from "./TripsClient"
+import getCurrentUser from "../actions/getCurrentUser"
+import getReservations from "../actions/getReservations"
+import ClientOnly from "../components/ClientOnly"
+import EmptyState from "../components/EmptyState"
+import TripsClient from "./TripsClient"
 
 
 
 
-// const TripsPages = async () =>{
-//     const currentUser = await getCurrentUser()
+const TripsPages = async () =>{
+    const currentUser = await getCurrentUser()
     
-//     if(!currentUser){
+    if(!currentUser){
 
-//     return(
+    return(
 
-//         <ClientOnly>
-//             <EmptyState
-//             title="Unauthorized"
-//             subtitle="Please login"
-//             />
-//             </ClientOnly>
-//     )
-//     }
+        <ClientOnly>
+            <EmptyState
+            title="Unauthorized"
+            subtitle="Please login"
+            />
+            </ClientOnly>
+    )
+    }
 
-// const reservations = await getReservations({
-//     userId: currentUser.id
-// })
+const reservations = await getReservations({
+    userId: currentUser.id
+})
 
-// if(reservations.length == 0 ){
-//     return(
-//         <ClientOnly>
-//             <EmptyState
-//             title="No tips found"
-//             subtitle="Looks like you have not reserved any trips"
+if(reservations.length == 0 ){
+    return(
+        <ClientOnly>
+            <EmptyState
+            title="No tips found"
+            subtitle="Looks like you have not reserved any trips"
             
-//             />
-//         </ClientOnly>
-//     )
+            />
+        </ClientOnly>
+    )
 
-// }
-// return(
-//     <ClientOnly>
-//         <TripsClient
-//         reservations={reservations}
-//         currentUser={currentUser}
-//         />
+}
+return(
+    <ClientOnly>
+        <TripsClient
+        reservations={reservations}
+        currentUser={currentUser}
+        />
 
-//     </ClientOnly>
-// )
-// }
+    </ClientOnly>
+)
+}
 
-// export default TripsPages
+export default TripsPages
