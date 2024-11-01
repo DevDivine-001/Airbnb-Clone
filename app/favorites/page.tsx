7b35f7fd2101fed2e70e36520e1c27184fbbdf13
Oscdivin <divineuwandu84@Gmail.com>
2024-11-01 22:54:24 +0100

    FavoritesClient page

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,36 +1,36 @@
-// import React from 'react'
-// import ClientOnly from '../components/ClientOnly'
-// import EmptyState from '../components/EmptyState'
-// import getCurrentUser from '../actions/getCurrentUser'
-// import getFavoriteListings from '../actions/getFavoritesListings'
-// import FavoritesClient from './FavoritesClient'
+import React from 'react'
+import ClientOnly from '../components/ClientOnly'
+import EmptyState from '../components/EmptyState'
+import getCurrentUser from '../actions/getCurrentUser'
+import getFavoriteListings from '../actions/getFavoritesListings'
+import FavoritesClient from './FavoritesClient'
 
 
 
-// const ListingPages =  async () => {
-//     const listings  = await getFavoriteListings()
-//     const currentUser = await getCurrentUser()
+const ListingPages =  async () => {
+    const listings  = await getFavoriteListings()
+    const currentUser = await getCurrentUser()
 
-//     if(listings.length == 0){
-//           return (
-//     <ClientOnly>
-//         <EmptyState
-//         title='No favorites found'
-//         subtitle='Looks like you have no favorites listings'
+    if(listings.length == 0){
+          return (
+    <ClientOnly>
+        <EmptyState
+        title='No favorites found'
+        subtitle='Looks like you have no favorites listings'
         
-//         />
-//     </ClientOnly>
-//   )
-//     }
+        />
+    </ClientOnly>
+  )
+    }
 
-//     return(
-//         <ClientOnly>
-//             <FavoritesClient
-//             listings={listings}
-//             currentUser={currentUser}
-//             />
-//         </ClientOnly>
-//     )
-// }
+    return(
+        <ClientOnly>
+            <FavoritesClient
+            listings={listings}
+            currentUser={currentUser}
+            />
+        </ClientOnly>
+    )
+}
 
-// export default ListingPages
+export default ListingPages
