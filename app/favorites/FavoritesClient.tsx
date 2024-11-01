d9ef4bcf0908e2063e5dbf6160b0ee3b53fca9a4
Oscdivin <divineuwandu84@Gmail.com>
2024-11-01 22:53:57 +0100

    FavoritesClient page

--- app/favorites/FavoritesClient.tsx
+++ app/favorites/FavoritesClient.tsx
@@ -1,45 +1,45 @@
-// import React from 'react'
-// import Container from '../components/Container'
-// import Heading from '../components/Heading'
-// import { SafeListing, SafeUser } from '../types/inex'
-// import ListingCard from '../components/Lisitings/ListingCard'
+import React from 'react'
+import Container from '../components/Container'
+import Heading from '../components/Heading'
+import { SafeListing, SafeUser } from '../types/inex'
+import ListingCard from '../components/Lisitings/ListingCard'
 
-// interface FavoritesClientProps{
-//     listings: SafeListing[]
-//     currentUser?: SafeUser | null
-// }
+interface FavoritesClientProps{
+    listings: SafeListing[]
+    currentUser?: SafeUser | null
+}
 
-// const FavoritesClient:React.FC<FavoritesClientProps> = ({
-//     listings,
-//     currentUser,
-// }) => {
-//   return (
-//     <Container>
-//         <Heading
-//         title="Favorites"
-//         subtitle='List of places you have favorites!'
-//         />
-//         <div className='
-//         mt-10
-//         grid
-//         grid-cols-1
-//         sm:grid-cols-2
-//         md:grid-cols-3
-//         lg:grid-cols-4
-//         xl:grid-cols-5
-//         2xl:grid-cols-6
-//         gap-8'>
-//             {listings.map((listing) =>(
-//                 <ListingCard
-//                 disabled
-//                 key={listing.id}
-//                 currentUser={currentUser}
-//                 data={listing}
-//                 />
-//             ))}
-//         </div>
-//     </Container>
-//   )
-// }
+const FavoritesClient:React.FC<FavoritesClientProps> = ({
+    listings,
+    currentUser,
+}) => {
+  return (
+    <Container>
+        <Heading
+        title="Favorites"
+        subtitle='List of places you have favorites!'
+        />
+        <div className='
+        mt-10
+        grid
+        grid-cols-1
+        sm:grid-cols-2
+        md:grid-cols-3
+        lg:grid-cols-4
+        xl:grid-cols-5
+        2xl:grid-cols-6
+        gap-8'>
+            {listings.map((listing) =>(
+                <ListingCard
+                disabled
+                key={listing.id}
+                currentUser={currentUser}
+                data={listing}
+                />
+            ))}
+        </div>
+    </Container>
+  )
+}
 
-// export default FavoritesClient
+export default FavoritesClient
