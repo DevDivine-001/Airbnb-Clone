6e147da60315b83ffa5a4b20b9fb59e99b6c5e2d
Oscdivin <divineuwandu84@Gmail.com>
2024-11-01 22:55:02 +0100

    FavoritesClient page with route

--- app/api/reservations/[reservationId]/route.ts
+++ app/api/reservations/[reservationId]/route.ts
@@ -1,36 +1,36 @@
-// import { NextResponse } from "next/server"
+import { NextResponse } from "next/server"
 
-// import getCurrentUser from "@/app/actions/getCurrentUser"
-// import prisma from "@/app/libs/primadb"
+import getCurrentUser from "@/app/actions/getCurrentUser"
+import prisma from "@/app/libs/primadb"
 
 
-// interface IParams {
-//     reservationId?: string;
-// }
+interface IParams {
+    reservationId?: string;
+}
 
-// export async function DELETE(request: Request,
-//     { params }: { params: IParams }) {
+export async function DELETE(request: Request,
+    { params }: { params: IParams }) {
 
-//     const currentUser = await getCurrentUser()
+    const currentUser = await getCurrentUser()
 
-//     if (!currentUser) {
-//         return NextResponse.error()
-//     }
+    if (!currentUser) {
+        return NextResponse.error()
+    }
 
-//     const { reservationId } = params
+    const { reservationId } = params
 
-//     if (!reservationId || typeof reservationId !== 'string') {
-//         throw new Error("Invalid ID")
-//     }
-//     const reservation = await prisma.reservation.deleteMany({
-//         where: {
-//             id: reservationId,
-//             OR: [
-//                 { userId: currentUser.id },
-//                 { listing: { userId: currentUser.id } }
-//             ]
-//         }
-//     });
+    if (!reservationId || typeof reservationId !== 'string') {
+        throw new Error("Invalid ID")
+    }
+    const reservation = await prisma.reservation.deleteMany({
+        where: {
+            id: reservationId,
+            OR: [
+                { userId: currentUser.id },
+                { listing: { userId: currentUser.id } }
+            ]
+        }
+    });
 
-//     return NextResponse.json(reservation)
-// }
+    return NextResponse.json(reservation)
+}
