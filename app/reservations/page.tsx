04d66ffc412713d5143420b6966289d14eea1cf8
Oscdivin <divineuwandu84@Gmail.com>
2024-11-01 22:51:11 +0100

    ReservationsClient page

--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -1,48 +1,48 @@
-// import React from 'react'
-// import ClientOnly from '../components/ClientOnly'
-// import EmptyState from '../components/EmptyState'
-// import getCurrentUser from '../actions/getCurrentUser'
-// import getReservations from '../actions/getReservations'
-// import ReservationsClient from './ReservationsClient'
+import React from 'react'
+import ClientOnly from '../components/ClientOnly'
+import EmptyState from '../components/EmptyState'
+import getCurrentUser from '../actions/getCurrentUser'
+import getReservations from '../actions/getReservations'
+import ReservationsClient from './ReservationsClient'
 
-// const ReservationPages  = async () => {
-//     const currentUser = await getCurrentUser()
-//  if(!currentUser){
-//      return (
-//     <ClientOnly>
-//         <EmptyState
-//         title='Unauthorized'
-//         subtitle='Please login'
-//         />
+const ReservationPages  = async () => {
+    const currentUser = await getCurrentUser()
+ if(!currentUser){
+     return (
+    <ClientOnly>
+        <EmptyState
+        title='Unauthorized'
+        subtitle='Please login'
+        />
         
-//     </ClientOnly>
-//   )
-//  }
+    </ClientOnly>
+  )
+ }
 
-//  const reservations = await getReservations({
-//     authorId: currentUser.id
+ const reservations = await getReservations({
+    authorId: currentUser.id
 
-//  })
+ })
 
-//  if(reservations.length == 0){
-//     return(
-//         <ClientOnly>
-//             <EmptyState
-//             title='No reservations found'
-//             subtitle='Looks like you have no reservations on your properties'/>
-//         </ClientOnly>
-//     )
-//  }
+ if(reservations.length == 0){
+    return(
+        <ClientOnly>
+            <EmptyState
+            title='No reservations found'
+            subtitle='Looks like you have no reservations on your properties'/>
+        </ClientOnly>
+    )
+ }
 
-//  return(
-//     <ClientOnly>
-//         <ReservationsClient
-//         reservations={reservations}
-//         currentUser={currentUser}
+ return(
+    <ClientOnly>
+        <ReservationsClient
+        reservations={reservations}
+        currentUser={currentUser}
         
-//         />
-//     </ClientOnly>
-//  )
-// }
+        />
+    </ClientOnly>
+ )
+}
 
-// export default ReservationPages
+export default ReservationPages
