26e38c257cf240fc3ccdbe7690c54aa07d913e5a
Oscdivin <divineuwandu84@Gmail.com>
2024-11-01 22:52:20 +0100

    PropertiesClient

--- app/properties/PropertiesClient.tsx
+++ app/properties/PropertiesClient.tsx
@@ -1,76 +1,76 @@
-// 'use client'
-// import React, { useCallback, useState } from 'react';
-// import Heading from '../components/Heading';
-// // import { SafeReservation, SafeUser } from '../types/index'; // Confirm correct file path
-// import Container from '../components/Container';
-// import { useRouter } from 'next/navigation';
-// import axios from 'axios';
-// import toast from 'react-hot-toast';
-// import { SafeListing, SafeReservation, SafeUser } from '../types/inex';
-// import ListingCard from '../components/Lisitings/ListingCard';
-// // import ListingCard from '../components/Listings/ListingCard';
+'use client'
+import React, { useCallback, useState } from 'react';
+import Heading from '../components/Heading';
+// import { SafeReservation, SafeUser } from '../types/index'; // Confirm correct file path
+import Container from '../components/Container';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import { SafeListing, SafeReservation, SafeUser } from '../types/inex';
+import ListingCard from '../components/Lisitings/ListingCard';
+// import ListingCard from '../components/Listings/ListingCard';
 
-// interface PropertiesClientProps {
-//   listings: SafeListing[];
-//   currentUser?: SafeUser | null;
-// }
+interface PropertiesClientProps {
+  listings: SafeListing[];
+  currentUser?: SafeUser | null;
+}
 
-// const TripsClient: React.FC<PropertiesClientProps> = ({
-//   listings,
-//   currentUser,
-// }) => {
-//   const router = useRouter();
-//   const [deletingId, setDeletingId] = useState('');
+const TripsClient: React.FC<PropertiesClientProps> = ({
+  listings,
+  currentUser,
+}) => {
+  const router = useRouter();
+  const [deletingId, setDeletingId] = useState('');
 
-//   const onCancel = useCallback(
-//     (id: string) => {
-//       setDeletingId(id);
+  const onCancel = useCallback(
+    (id: string) => {
+      setDeletingId(id);
 
-//       axios
-//         .delete(`/api/listings/${id}`)
-//         .then(() => {
-//           toast.success('Listing deleted');
-//           router.refresh();
-//         })
-//         .catch((error) => {
-//           toast.error(error?.response?.data?.error || 'Something went wrong');
-//         })
-//         .finally(() => {
-//           setDeletingId('');
-//         });
-//     },
-//     [router]
-//   );
+      axios
+        .delete(`/api/listings/${id}`)
+        .then(() => {
+          toast.success('Listing deleted');
+          router.refresh();
+        })
+        .catch((error) => {
+          toast.error(error?.response?.data?.error || 'Something went wrong');
+        })
+        .finally(() => {
+          setDeletingId('');
+        });
+    },
+    [router]
+  );
 
-//   return (
-//     <Container>
-//       <Heading title="Properties" subtitle="List of your properties" />
-//       <div
-//         className="
-//           mt-10
-//           grid
-//           grid-cols-1
-//           sm:grid-cols-2
-//           md:grid-cols-3
-//           lg:grid-cols-4
-//           xl:grid-cols-5
-//           2xl:grid-cols-6
-//           gap-8"
-//       >
-//         {listings.map((listing) => (
-//           <ListingCard
-//             key={listing.id}
-//             data={listing}
-//             actionId={listing.id}
-//             onAction={onCancel}
-//             disabled={deletingId === listing.id}
-//             actionLabel="Delete property"
-//             currentUser={currentUser}
-//           />
-//         ))}
-//       </div>
-//     </Container>
-//   );
-// };
+  return (
+    <Container>
+      <Heading title="Properties" subtitle="List of your properties" />
+      <div
+        className="
+          mt-10
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8"
+      >
+        {listings.map((listing) => (
+          <ListingCard
+            key={listing.id}
+            data={listing}
+            actionId={listing.id}
+            onAction={onCancel}
+            disabled={deletingId === listing.id}
+            actionLabel="Delete property"
+            currentUser={currentUser}
+          />
+        ))}
+      </div>
+    </Container>
+  );
+};
 
-// export default TripsClient;
+export default TripsClient;
