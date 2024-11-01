ca4ebc33e9e24c0bb8e3b2296b03d98ac1d2fbab
Oscdivin <divineuwandu84@Gmail.com>
2024-11-01 22:46:17 +0100

    Page error resolve

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,57 @@
-// import getCurrentUser from "./actions/getCurrentUser"
-// import { getListings, IListingsParams } from "./actions/getLisitings"
-// import ClientOnly from "./components/ClientOnly"
-// import Container from "./components/Container"
-// import EmptyState from "./components/EmptyState"
-// import ListingCard from "./components/Lisitings/ListingCard"
+import getCurrentUser from "./actions/getCurrentUser"
+import { getListings, IListingsParams } from "./actions/getLisitings"
+import ClientOnly from "./components/ClientOnly"
+import Container from "./components/Container"
+import EmptyState from "./components/EmptyState"
+import ListingCard from "./components/Lisitings/ListingCard"
 
-// interface HomeProps{
-//   searchParams: IListingsParams
-// }
+interface HomeProps{
+  searchParams: IListingsParams
+}
 
-// const page = async ({searchParams}: HomeProps) => {
+const page = async ({searchParams}: HomeProps) => {
 
-//   const listings = await getListings(searchParams)
-//   const currentUser = await getCurrentUser()
+  const listings = await getListings(searchParams)
+  const currentUser = await getCurrentUser()
 
-//   // const isEmpty = true
+  // const isEmpty = true
 
-//   if(listings.length == 0){
-//   return(
-//       <ClientOnly>
-//       <EmptyState showReset/>
-//     </ClientOnly>
-//   )
-//   }
-//   return (
-//     <ClientOnly>
-//       <Container>
-//       <div className='
-//     pt-24
-//     grid
-//     grid-cols-1
-//     sm:grid-cols-2
-//     md:grid-cols-3
-//     lg:grid-cols-4
-//     xl:grid-cols-5
-//     2xl:grid-cols-6
-//     gap-8'>
-//       {listings.map((listing) =>{
-//         return(
-//           <ListingCard
-//           currentUser={currentUser}
-//           key={listing.id}
-//             data={listing}
-//             disabled   
-//          />
+  if(listings.length == 0){
+  return(
+      <ClientOnly>
+      <EmptyState showReset/>
+    </ClientOnly>
+  )
+  }
+  return (
+    <ClientOnly>
+      <Container>
+      <div className='
+    pt-24
+    grid
+    grid-cols-1
+    sm:grid-cols-2
+    md:grid-cols-3
+    lg:grid-cols-4
+    xl:grid-cols-5
+    2xl:grid-cols-6
+    gap-8'>
+      {listings.map((listing) =>{
+        return(
+          <ListingCard
+          currentUser={currentUser}
+          key={listing.id}
+            data={listing}
+            disabled   
+         />
 
        
-//         )
-//       })}
-//     </div>
-//     </Container>
-//     </ClientOnly>
-//   )
-// }
+        )
+      })}
+    </div>
+    </Container>
+    </ClientOnly>
+  )
+}
 
-// export default page
+export default page
