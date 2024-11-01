e41894d41694a96eb507e79f6bf9183c6101814a
Oscdivin <divineuwandu84@Gmail.com>
2024-11-01 22:58:35 +0100

    FavoritesClient page with for favorties route

--- app/actions/getFavoritesListings.ts
+++ app/actions/getFavoritesListings.ts
@@ -1,32 +1,32 @@
-// import prisma from '@/app/libs/primadb'
-// import getCurrentUser from './getCurrentUser'
+import prisma from '@/app/libs/primadb'
+import getCurrentUser from './getCurrentUser'
 
-// export default async function getFavoriteListings() {
-//     try {
-//         const currentUser = await getCurrentUser()
+export default async function getFavoriteListings() {
+    try {
+        const currentUser = await getCurrentUser()
 
-//         if (!currentUser) {
-//             return []
-//         }
+        if (!currentUser) {
+            return []
+        }
 
-//         const favorites = await prisma.listing.findMany({
-//             where: {
-//                 id: {
-//                     in: [... (currentUser.favoriteIds || [])]
-//                 }
-//             }
-//         })
+        const favorites = await prisma.listing.findMany({
+            where: {
+                id: {
+                    in: [... (currentUser.favoriteIds || [])]
+                }
+            }
+        })
 
-//         const safeFavorites = favorites.map((favorite) => ({
-//             ...favorite,
-//             createdAt: favorite.createdAt.toISOString()
-//         }))
+        const safeFavorites = favorites.map((favorite) => ({
+            ...favorite,
+            createdAt: favorite.createdAt.toISOString()
+        }))
 
-//         return safeFavorites;
+        return safeFavorites;
 
-//     } catch (error: any) {
-//         throw new Error(error)
-//     }
-// }
+    } catch (error: any) {
+        throw new Error(error)
+    }
+}
 
 
