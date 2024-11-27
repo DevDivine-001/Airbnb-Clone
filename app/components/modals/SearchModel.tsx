a14fcd8ecf874e7c9f55251da04e8b44435b44b3
Oscdivin <divineuwandu84@Gmail.com>
2024-11-27 11:05:27 +0100

    fix the error in Eslint

--- app/components/modals/SearchModel.tsx
+++ app/components/modals/SearchModel.tsx
@@ -36,31 +36,29 @@ const SearchModel = () => {
     })
 
     const Map = useMemo(() => dynamic(() => import("../Map"),{
-ssr: false
-    }), [location])
+        ssr: false
+    }), [])  // Removed `location` from dependencies
 
-    const onBack = useCallback(() =>{
+    const onBack = useCallback(() => {
         setStep((value) => value - 1)
-
     }, [])
-    const onNext = useCallback(() =>{
+
+    const onNext = useCallback(() => {
         setStep((value) => value + 1)
-
     }, [])
 
-    const onSubmit = useCallback( async() =>{
-
-        if(step !== STEPS.INFO){
+    const onSubmit = useCallback( async() => {
+        if (step !== STEPS.INFO) {
             return onNext()
         }
 
         let currentQuery = {}
 
-        if(params){
-            currentQuery = qs.parse(params .toString()
-            )
+        if (params) {
+            currentQuery = qs.parse(params.toString())
         }
-        const updatedQuery: any ={
+
+        const updatedQuery: any = {
             ...currentQuery,
             locationValue: location?.value,
             guestCount,
@@ -68,21 +66,20 @@ ssr: false
             bathroomCount
         }
 
-        if(dateRange.startDate){
+        if (dateRange.startDate) {
             updatedQuery.startDate = formatISO(dateRange.startDate)
         }
-        if(dateRange.endDate){
+        if (dateRange.endDate) {
             updatedQuery.endDate = formatISO(dateRange.endDate)
         }
 
         const url = qs.stringifyUrl({
-            url:"/",
+            url: "/",
             query: updatedQuery
-        }, {skipNull:true})
+        }, { skipNull: true })
 
         setStep(STEPS.LOCATION)
         searchModal.onClose()
-
         route.push(url)
     }, [
         step,
@@ -97,91 +94,280 @@ ssr: false
         params
     ])
 
-    
-    const actionLabel = useMemo(() =>{
-        if(step == STEPS.INFO){
+    const actionLabel = useMemo(() => {
+        if (step === STEPS.INFO) {
             return 'Search'
         }
         return 'Next'
-    },[step])
-    const secondaryActionLabel = useMemo(() =>{
-        if(step == STEPS.LOCATION){
+    }, [step])
+
+    const secondaryActionLabel = useMemo(() => {
+        if (step === STEPS.LOCATION) {
             return undefined
         }
         return 'Back'
-    },[step])
+    }, [step])
 
     let bodyContent = (
         <div className='flex flex-col gap-3.5 '>
             <Heading
-            title='Where do you wanna go?'
-            subtitle='Find the perfect location'
+                title='Where do you wanna go?'
+                subtitle='Find the perfect location'
             />
             <CountrySelect
-            value={location}
-            onChange={(value) => setLocation(value  as CountrySelectValue)}/>
+                value={location}
+                onChange={(value) => setLocation(value as CountrySelectValue)}
+            />
             <hr />
-            <Map center={location?.latlng}/>
-
+            <Map center={location?.latlng} />
         </div>
     )
 
-    if(step == STEPS.DATE){
-bodyContent =(
-    <div>
-        <Heading
-        title='When do you plan to go?'
-        subtitle='Make sure everyone is free!'
-        />
-        <Calendar
-        value={dateRange}
-        onChange={(value) => setDateRange(value.selection)}/>
-    </div>
-)
+    if (step === STEPS.DATE) {
+        bodyContent = (
+            <div>
+                <Heading
+                    title='When do you plan to go?'
+                    subtitle='Make sure everyone is free!'
+                />
+                <Calendar
+                    value={dateRange}
+                    onChange={(value) => setDateRange(value.selection)}
+                />
+            </div>
+        )
     }
 
+    if (step === STEPS.INFO) {
+        bodyContent = (
+            <div className='flex flex-col gap-8'>
+                <Heading
+                    title='More information'
+                    subtitle='Find your perfect place'
+                />
+                <Counter
+                    title='Guests'
+                    subtitle='How many guests are coming'
+                    value={guestCount}
+                    onChange={(value) => setGuestCount(value)}
+                />
+                <Counter
+                    title='Rooms'
+                    subtitle='How many rooms do you need'
+                    value={roomCount}
+                    onChange={(value) => setRoomCount(value)}
+                />
+                <Counter
+                    title='Bathrooms'
+                    subtitle='How many bathrooms do you need'
+                    value={bathroomCount}
+                    onChange={(value) => setBathroomCount(value)}
+                />
+            </div>
+        )
+    }
 
- if(step == STEPS.INFO){
-    bodyContent = (
-        <div className='flex flex-col gap-8'>
-            <Heading
-            title='M`ore information'
-            subtitle='Find your perfect place'
-            />
-            <Counter
-            title='Guests'
-            subtitle='How many guests are coming'
-            value={guestCount}
-            onChange={(value) => setGuestCount(value)}
-            />
-            <Counter
-            title='Room'
-            subtitle='How many rooms do you need'
-            value={roomCount}
-            onChange={(value) => setRoomCount(value)}
-            />
-            <Counter
-            title='Bathrooms'
-            subtitle='How many bathrooms do you need'
-            value={bathroomCount}
-            onChange={(value) => setBathroomCount(value)}
-            />
-        </div>
+    return (
+        <Modal
+            isOpen={searchModal.isOpen}
+            onClose={searchModal.onClose}
+            onSubmit={onSubmit}
+            title='Filters'
+            actionLabel={actionLabel}
+            secondaryActionLabel={secondaryActionLabel}
+            secondaryAction={step === STEPS.LOCATION ? undefined : onBack}
+            body={bodyContent}
+        />
     )
-
- }
-  return (
-    <Modal
-    isOpen={searchModal.isOpen}
-    onClose={searchModal.onClose}
-    onSubmit={onSubmit}
-    title='Filters'
-    actionLabel={actionLabel}
-    secondaryActionLabel={secondaryActionLabel}
-    secondaryAction={step == STEPS.LOCATION ? undefined : onBack}
-    body={bodyContent}
-    />
-  )
 }
 
 export default SearchModel
+
+
+// 'use client'
+
+// import React, { useCallback, useMemo, useState } from 'react'
+// import qs from 'query-string'
+// import Modal from './Modal'
+// import useSearchModel from '../hooks/useLSearchModal'
+// import { useRouter, useSearchParams } from 'next/navigation'
+// import dynamic from 'next/dynamic'
+// import CountrySelect, { CountrySelectValue } from '../input/CountrySelect'
+// import { formatISO } from 'date-fns'
+// import Heading from '../Heading'
+// // import { Range } from 'react-date-range'
+// import Calendar from '../input/Calendar'
+// import Counter from '../input/Counter'
+
+// enum STEPS{
+//     LOCATION =0,
+//     DATE =1,
+//     INFO = 2,
+// }
+
+// const SearchModel = () => {
+//     const route = useRouter()
+//     const params = useSearchParams()
+//     const searchModal = useSearchModel()
+    
+//     const [location, setLocation] = useState<CountrySelectValue>()
+//     const [step, setStep] = useState(STEPS.LOCATION)
+//     const [guestCount, setGuestCount] = useState(1)
+//     const [roomCount, setRoomCount] = useState(1)
+//     const [bathroomCount, setBathroomCount] = useState(1)
+//     const [dateRange, setDateRange] = useState<any> ({
+//         startDate: new Date(),
+//         endDate: new Date(),
+//         key: 'selection',
+//     })
+
+//     const Map = useMemo(() => dynamic(() => import("../Map"),{
+// ssr: false
+//     }), [location])
+
+//     const onBack = useCallback(() =>{
+//         setStep((value) => value - 1)
+
+//     }, [])
+//     const onNext = useCallback(() =>{
+//         setStep((value) => value + 1)
+
+//     }, [])
+
+//     const onSubmit = useCallback( async() =>{
+
+//         if(step !== STEPS.INFO){
+//             return onNext()
+//         }
+
+//         let currentQuery = {}
+
+//         if(params){
+//             currentQuery = qs.parse(params .toString()
+//             )
+//         }
+//         const updatedQuery: any ={
+//             ...currentQuery,
+//             locationValue: location?.value,
+//             guestCount,
+//             roomCount,
+//             bathroomCount
+//         }
+
+//         if(dateRange.startDate){
+//             updatedQuery.startDate = formatISO(dateRange.startDate)
+//         }
+//         if(dateRange.endDate){
+//             updatedQuery.endDate = formatISO(dateRange.endDate)
+//         }
+
+//         const url = qs.stringifyUrl({
+//             url:"/",
+//             query: updatedQuery
+//         }, {skipNull:true})
+
+//         setStep(STEPS.LOCATION)
+//         searchModal.onClose()
+
+//         route.push(url)
+//     }, [
+//         step,
+//         searchModal,
+//         location,
+//         route,
+//         guestCount,
+//         roomCount,
+//         bathroomCount,
+//         dateRange,
+//         onNext,
+//         params
+//     ])
+
+    
+//     const actionLabel = useMemo(() =>{
+//         if(step == STEPS.INFO){
+//             return 'Search'
+//         }
+//         return 'Next'
+//     },[step])
+//     const secondaryActionLabel = useMemo(() =>{
+//         if(step == STEPS.LOCATION){
+//             return undefined
+//         }
+//         return 'Back'
+//     },[step])
+
+//     let bodyContent = (
+//         <div className='flex flex-col gap-3.5 '>
+//             <Heading
+//             title='Where do you wanna go?'
+//             subtitle='Find the perfect location'
+//             />
+//             <CountrySelect
+//             value={location}
+//             onChange={(value) => setLocation(value  as CountrySelectValue)}/>
+//             <hr />
+//             <Map center={location?.latlng}/>
+
+//         </div>
+//     )
+
+//     if(step == STEPS.DATE){
+// bodyContent =(
+//     <div>
+//         <Heading
+//         title='When do you plan to go?'
+//         subtitle='Make sure everyone is free!'
+//         />
+//         <Calendar
+//         value={dateRange}
+//         onChange={(value) => setDateRange(value.selection)}/>
+//     </div>
+// )
+//     }
+
+
+//  if(step == STEPS.INFO){
+//     bodyContent = (
+//         <div className='flex flex-col gap-8'>
+//             <Heading
+//             title='M`ore information'
+//             subtitle='Find your perfect place'
+//             />
+//             <Counter
+//             title='Guests'
+//             subtitle='How many guests are coming'
+//             value={guestCount}
+//             onChange={(value) => setGuestCount(value)}
+//             />
+//             <Counter
+//             title='Room'
+//             subtitle='How many rooms do you need'
+//             value={roomCount}
+//             onChange={(value) => setRoomCount(value)}
+//             />
+//             <Counter
+//             title='Bathrooms'
+//             subtitle='How many bathrooms do you need'
+//             value={bathroomCount}
+//             onChange={(value) => setBathroomCount(value)}
+//             />
+//         </div>
+//     )
+
+//  }
+//   return (
+//     <Modal
+//     isOpen={searchModal.isOpen}
+//     onClose={searchModal.onClose}
+//     onSubmit={onSubmit}
+//     title='Filters'
+//     actionLabel={actionLabel}
+//     secondaryActionLabel={secondaryActionLabel}
+//     secondaryAction={step == STEPS.LOCATION ? undefined : onBack}
+//     body={bodyContent}
+//     />
+//   )
+// }
+
+// export default SearchModel
