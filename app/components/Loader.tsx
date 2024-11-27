3a2e49836284561bc1620fb558ec46612d8150e6
Oscdivin <divineuwandu84@Gmail.com>
2024-11-27 10:57:37 +0100

    Loeder

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -1,26 +1,25 @@
-'Use client'
+'use client'
 import React from 'react'
-import {BounceLoader, PuffLoader} from "react-spinners"
+// import {BounceLoader, PuffLoader} from "react-spinners"
+import { BallTriangle } from "react-loader-spinner"
 
 const Loader = () => {
   return (
     <div
-    className='h-[70vh]
-    flex
-    flex-col
-    justify-center
-    items-center'
+      className='h-[70vh] flex flex-col justify-center items-center'
     >
-        {/* <PuffLoader
-        
-        
-        /> */}
-        <BounceLoader
-        size={100}
-        color="red"
-         />
+        {/* <PuffLoader /> */}
+        {/* <ClapSpinner size={100} color="red" /> */}
+        <BallTriangle 
+          height={100}
+          width={100}
+          radius={5}
+          color="#F43F5E"
+        />
     </div>
   )
 }
 
 export default Loader
+
+
--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -67,10 +67,14 @@ const RentModal = () => {
     const bathroomCount = watch('bathroomCount')
     const imageSrc = watch('imageSrc')
 
-    const Map = useMemo(() => dynamic(() => import('../Map'), {
-        ssr: false
+    // const Map = useMemo(() => dynamic(() => import('../Map'), {
+    //     ssr: false
 
-    }), [location])
+    // }), [location])
+
+    const Map = dynamic(() => import('../Map'), {
+        ssr: false
+    });
 
     const setCustomValue = (id: string, value:any) =>{
         setValue(id, value, {
