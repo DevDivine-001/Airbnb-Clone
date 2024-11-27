16e13516551adcc678ad8c28db865f59c1101e88
Oscdivin <divineuwandu84@Gmail.com>
2024-11-27 11:46:49 +0100

    adde a line of code in net.config.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     experimental: {
-//         appDir: true,
 
-//     },
-//     images: {
-//         domains: [
-//             "avatars.githubusercontent.com"
-//         ]
-//     }
-// };
-
-// export default nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     images: {
-//         remotePatterns: [
-//             {
-//                 protocol: 'https',
-//                 hostname: 'avatars.githubusercontent.com',
-//                 pathname: '/**',
-//             },
-//         ],
-//     },
-// };
-
-// export default nextConfig;
-
-//** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "standalone",
     images: {
         remotePatterns: [
             {
